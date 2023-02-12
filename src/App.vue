057a09e4f4e1979b2eb6156330ef22e77dfdab99
codewizz_ubuntu <c.chaiwat.ch@gmail.com>
2023-02-12 13:47:24 +0700

    add: new view and routing to it

--- src/App.vue
+++ src/App.vue
@@ -5,16 +5,14 @@ import { RouterLink, RouterView } from 'vue-router'
 <template>
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
       <div class="container-fluid">
-        <a class="navbar-brand" href="#">Navbar</a>
+        <a class="navbar-brand" href="#">MyCat</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
           <div class="navbar-nav">
-            <a class="nav-link active" aria-current="page" href="#">Home</a>
-            <a class="nav-link" href="#">Features</a>
-            <a class="nav-link" href="#">Pricing</a>
-            <a class="nav-link disabled">Disabled</a>
+            <RouterLink to="/" class="nav-link">Home</RouterLink>
+            <RouterLink to="/about" class="nav-link">About</RouterLink>
           </div>
         </div>
       </div>
---
+++ src/views/CatListViews.vue
@@ -0,0 +1,8 @@
+<script setup>
+</script>
+
+<template>
+  <div class="row">
+    <h1>My Cat Here</h1>
+  </div>
+</template>
