6208c6fec9fb12798108fae2b4482e2fe6d36f9b
codewizz_ubuntu <c.chaiwat.ch@gmail.com>
2023-02-12 14:28:29 +0700

    update: use props

--- src/components/CatCard.vue
+++ src/components/CatCard.vue
@@ -1,13 +1,11 @@
 <script setup>
 import { ref } from 'vue'; 
 
-const cat = ref({
-  id: 32413,
-  name: "Choke",
-  age: "2 yrs",
-  weight: "5 kg",
-  favoriteFood: "shrimp",
-  img: "https://cdn.shopify.com/s/files/1/2195/6819/files/Screen_Shot_2020-04-14_at_3.46.02_PM_large.png?v=1586854010"
+defineProps({
+  cat: {
+    type: Object,
+    required: true,
+  }
 })
 
 </script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,22 @@
 <script setup>
-import CatListViews from '@/components/CatListViews.vue'
+import CatCard from '@/components/CatCard.vue'
+import { ref } from 'vue'
+
+const cats = ref([
+  {
+    id: 32413,
+    name: "Choke",
+    age: "2 yrs",
+    weight: "5 kg",
+    favoriteFood: "shrimp",
+    img: "https://cdn.shopify.com/s/files/1/2195/6819/files/Screen_Shot_2020-04-14_at_3.46.02_PM_large.png?v=1586854010"
+  }
+])
 </script>
 
 <template>
   <div class="row">
     <h1>My Cat Here</h1>
-    <CatListViews />
-    <CatListViews />
-    <CatListViews />
+    <CatCard />
   </div>
 </template>
