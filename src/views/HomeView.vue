f4ba10c2549ddc84128ecf4b10af8940f2799ec4
codewizz_ubuntu <c.chaiwat.ch@gmail.com>
2023-02-13 08:18:20 +0700

    add: more cat

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -9,7 +9,23 @@ const cats = ref([
     age: "2 yrs",
     weight: "5 kg",
     favoriteFood: "shrimp",
-    img: "https://cdn.shopify.com/s/files/1/2195/6819/files/Screen_Shot_2020-04-14_at_3.46.02_PM_large.png?v=1586854010"
+    img: "https://cdn-fastly.petguide.com/media/2022/02/16/8260283/scottish-straight.jpg?size=1200x628"
+  },
+  {
+    id: 32414,
+    name: "Heng",
+    age: "4 yrs",
+    weight: "7 kg",
+    favoriteFood: "What ever",
+    img: "https://penntoday.upenn.edu/sites/default/files/2020-02/cat-behavior-social.jpg"
+  },
+  {
+    id: 32415,
+    name: "Thao",
+    age: "N/A",
+    weight: "4 kg",
+    favoriteFood: "What ever",
+    img: "https://www.bubblypet.com/wp-content/uploads/2021/11/Beautiful-grey-cat-breed-portrait.jpg"
   }
 ])
 </script>
@@ -17,6 +33,6 @@ const cats = ref([
 <template>
   <div class="row">
     <h1>My Cat Here</h1>
-    <CatCard />
+    <CatCard v-for="cat in cats" :key="cat.id" :cat="cat"/>
   </div>
 </template>
